16fb1e116100b27b4d950870f214a7f3cda07e51
gvg8 <gvg8@hi.is>
2021-03-04 12:48:19 +0000

    Button test successful
    Successfully connected a button to a function in SampleController.java.
    We now know how to assign functions to buttons.
    Next we need to learn how a function can read values from textfields and
    how it can create and append an object to the scene
    (such as adding a label object to the result area)

--- scenetest/src/application/SampleController.java
+++ scenetest/src/application/SampleController.java
@@ -1,5 +1,21 @@
 package application;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+public class SampleController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		// Nothing to see here, move along.
+	}
+	
+	public void doSomething(ActionEvent event) {
+		System.out.println("Button clicked!");
+	}
 	
 }
