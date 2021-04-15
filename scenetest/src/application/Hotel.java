2c71c1b638a7171b2c3cf51478b93bf81d825a95
spg6-hi <spg6@hi.is>
2021-04-15 15:33:00 +0000

    smá lagfæring

--- scenetest/src/application/Hotel.java
+++ scenetest/src/application/Hotel.java
@@ -3,7 +3,6 @@ package application;
 public class Hotel{
 	int hotelId;
 	String hotelName;
-<<<<<<< HEAD
 	String hotelAddress;
 	String location;
 	public Hotel(int id, String name, String address, String loc) {
@@ -11,33 +10,21 @@ public class Hotel{
 		hotelName = name;
 		hotelAddress = address;
 		location = loc;
-=======
-	String location;
-	public Hotel(int id, String name, String loc) {
-		this.hotelId = id;
-		this.hotelName = name;
-		this.location = loc;
->>>>>>> refs/remotes/origin/main
 	}
 	
+	int getId() {
+		return hotelId;
+	}
 	
 	String getHotelName() {
-<<<<<<< HEAD
 		return hotelName;
 	}
 	
 	String getAddress() {
 		return hotelAddress;
-=======
-		return this.hotelName;
->>>>>>> refs/remotes/origin/main
 	}
 	
 	String getLocation() {
-<<<<<<< HEAD
 		return location;
-=======
-		return this.location;
->>>>>>> refs/remotes/origin/main
 	}
 }
