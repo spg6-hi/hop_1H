46608f956e0a239eca1cefbad8e40e8217425f7f
spg6-hi <spg6@hi.is>
2021-04-19 21:05:24 +0000

    search methods í DBmanager nokkurn vegin komin

--- scenetest/src/application/DBManager.java
+++ scenetest/src/application/DBManager.java
@@ -15,6 +15,10 @@ public class DBManager {
 		return connection;
 	}
 	
+	public String DateReverse(String dma) {
+        return dma.substring(dma.length()-2,dma.length()) + dma.substring(dma.length()-5,dma.length()-3) + dma.substring(0, 4);
+    }
+	
 	void addHotel(String name, String address, String location) throws SQLException {
 		Connection conn = connect();
 		PreparedStatement stmt = conn.prepareStatement("insert into `Hotels` values(default,?,?,?)");
@@ -24,11 +28,23 @@ public class DBManager {
 		stmt.executeUpdate();
 	}
 	
-	Stack<Hotel> searchByName(String name) throws SQLException{
+	
+	
+	
+	Stack<Hotel> search(String name ,String location) throws SQLException{
 		Stack<Hotel> results = new Stack<Hotel>();
 		Connection conn = connect();
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM `Hotels` WHERE `name` = ?");
+		String query = "SELECT * FROM `Hotels` WHERE ";
+        if(name != "null") query += "`name` = ? AND ";
+        if(location != "null") query += "`location` = ? AND ";
+        if(query.substring(query.length() - 4, query.length()).equals("AND ")) query = query.substring(0, query.length() - 5);
+		PreparedStatement stmt = conn.prepareStatement(query);
+		
 		stmt.setString(1, name);
+		if(name == "null" && location != "null") stmt.setString(1, location);
+		if(name != "null" && location != "null") stmt.setString(2, location);
+		
+		
 		ResultSet rs = stmt.executeQuery();
 		while(rs.next())
 	    {
@@ -42,22 +58,22 @@ public class DBManager {
 		return results;
 	}
 	
-	
-	Stack<Hotel> searchByLocation(String location) throws SQLException{
-		Stack<Hotel> results = new Stack<Hotel>();
+	Stack<Room> getRooms(String hotelId) throws SQLException{
+		Stack<Room> results = new Stack<Room>();
 		Connection conn = connect();
-		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM `Hotels` WHERE `location` = ?");
-		stmt.setString(1, location);
+		PreparedStatement stmt = conn.prepareStatement("SELECT * FROM `rooms` WHERE `hotelid` = ? AND `user` = null");
+		stmt.setString(1, hotelId);
 		ResultSet rs = stmt.executeQuery();
+		
 		while(rs.next())
 	    {
-			results.push(new Hotel(rs.getInt("id"), 
-								   rs.getString("name"), 
-								   rs.getString("address"), 
-								   rs.getString("location")
+			results.push(new Room(rs.getInt("hotelid"),
+								  rs.getInt("roomid"),
+								  rs.getString("date"),
+								  rs.getString("user"),
+								  rs.getString("roomtype")
 						));
 	    }
-	    rs.close();
 		return results;
 	}
 	
--- scenetest/src/application/UserController.java
+++ scenetest/src/application/UserController.java
@@ -4,25 +4,31 @@ import java.sql.SQLException;
 import java.util.Stack;
 
 public class UserController {
-	DBManager dbManager = new DBManager();
+	static DBManager dbManager = new DBManager();
 	
 	Stack<Hotel> getHotels() throws SQLException{
 		return dbManager.getHotelList();
 	}
 	
-	void deleteHotel(Hotel hotel) {
-		dbManager.removeHotel(hotel);
+	static Stack<Hotel> search(String name, String loacation) throws SQLException{
+		return dbManager.search(name, loacation);
 	}
+
 	
-	void newHotel(String hotelName, String hotelAddress, String location) throws SQLException {
+	static void newHotel(String hotelName, String hotelAddress, String location) throws SQLException {
 		dbManager.addHotel(hotelName, hotelAddress, location);
 	}
 	
-	Stack<Hotel> searchByName(String name) throws SQLException{
-		return dbManager.searchByName(name);
-	}
-	
-	Stack<Hotel> searchByLocation(String location) throws SQLException{
-		return dbManager.searchByLocation(location);
+	public static void main(String[] args) throws ClassNotFoundException, SQLException
+	{
+		/*for(int i=0;i < 4; i++) {
+			newHotel("testhotel" + String.valueOf(i), "testaddress"+ String.valueOf(i), "testlocation"+ String.valueOf(i));
+		}*/
+		
+		Stack<Hotel> hotels = search("null", "testlocation0");
+		for(int i=0;i < hotels.size(); i++) {
+			System.out.println(hotels.pop().getHotelName());
+		}
+		
 	}
 }
